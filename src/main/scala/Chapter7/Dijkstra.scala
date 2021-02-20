cf989859c00206854d0c8e08dce364af372a8e46
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-02-20 19:09:26 +0000

    Second Iteration

--- src/main/scala/Chapter7/Dijkstra.scala
+++ src/main/scala/Chapter7/Dijkstra.scala
@@ -2,47 +2,44 @@ package Chapter7
 
 import scala.annotation.tailrec
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
-//First Iteration
+//Second Iteration
 object Dijkstra {
-
-  val processed = new ArrayBuffer[String]()
-
-  def apply(graph: Map[String, Map[String, Double]]): Int = {
-
-    val costs: mutable.Map[String, Double] = mutable.Map[String, Double]("fin" -> Double.PositiveInfinity) ++ graph(
-      "start")
+  def apply(graph: Map[String, List[Node]]): Int = {
+    val costs: List[Node]                    = graph("start") ++ List(Node("fin", Double.PositiveInfinity))
     val parents: mutable.Map[String, String] = mutable.Map[String, String]()
-    graph("start").keys.foreach { value =>
-      parents.addOne(value -> "start")
+    graph("start").foreach { value =>
+      parents.addOne(value.name -> "start")
     }
+
     @tailrec
-    def recurse: Int =
-      lowestCostNode(costs.toMap) match {
+    def recurse(processedNodes: List[String]): Int = {
+      val unprocessedNodes: List[Node] = costs.filterNot(node => processedNodes.contains(node.name))
+      val lowestCostNode = unprocessedNodes match {
+        case Nil          => None
+        case head :: tail => Some((head :: tail).minBy(_.edge))
+      }
+
+      lowestCostNode match {
         case Some(node) => {
-          val neighbours: Option[Map[String, Double]] = graph.get(node)
+          val neighbours: Option[List[Node]] = graph.get(node.name)
           neighbours.foreach { neighbour =>
             neighbour.foreach { n =>
-              if (costs(n._1) > costs(node) + n._2) {
-                costs.update(n._1, costs(node) + n._2)
-                parents.update(n._1, node)
+              val currentNode = costs.find(_.name == n.name)
+              val currentCost = currentNode.get.edge
+              val newCost     = costs.find(_.name == node.name).get.edge + n.edge
+              if (currentCost > newCost) {
+                currentNode.get.edge = newCost
+                parents.update(n.name, node.name)
               }
             }
           }
-          processed.addOne(node)
-          recurse
+          recurse(node.name :: processedNodes)
         }
-        case None => costs("fin").toInt
+        case None => costs.find(_.name == "fin").get.edge.toInt
       }
-    recurse
-  }
+    }
 
-  def lowestCostNode(costs: Map[String, Double]): Option[String] = {
-    val unprocessedNodes = costs.filterNot(value => processed.contains(value._1))
-    if (unprocessedNodes.nonEmpty) {
-      Some(unprocessedNodes.minBy(_._2)._1)
-    } else
-      None
+    recurse(List.empty)
   }
 }
--- src/test/scala/chapter7/DijkstraSpec.scala
+++ src/test/scala/chapter7/DijkstraSpec.scala
@@ -1,30 +1,32 @@
 package chapter7
 
-import Chapter7.Dijkstra
+import Chapter7.{Dijkstra, Node}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class DijkstraSpec extends AnyFlatSpec with Matchers {
 
-  "Dijkstra" should "return the shortest path" in {
-    val startNeighbours = Map[String, Double]("a"   -> 6, "b" -> 2)
-    val aNeighbours     = Map[String, Double]("fin" -> 1)
-    val bNeighbours     = Map[String, Double]("a"   -> 3, "fin" -> 5)
+  "Dijkstra" should "return the shortest path as 2" in {
+    val startNeighbours = List[Node](Node("a", 1), Node("b", 2))
+    val aNeighbours     = List[Node](Node("fin", 1))
+    val bNeighbours     = List[Node](Node("a", 3), Node("fin", 5))
 
-    val graph: Map[String, Map[String, Double]] =
+    val graph: Map[String, List[Node]] =
+      Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours)
+
+    Dijkstra(graph) shouldBe 2
+  }
+
+  "Dijkstra" should "return the shortest path" in {
+    val startNeighbours = List[Node](Node("a", 6), Node("b", 2))
+    val aNeighbours     = List[Node](Node("fin", 1))
+    val bNeighbours     = List[Node](Node("a", 3), Node("fin", 5))
+
+    val graph: Map[String, List[Node]] =
       Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours)
 
     Dijkstra(graph) shouldBe 6
   }
 
-  "Dijkstra" should "return the shortest path as 2" in {
-    val startNeighbours = Map[String, Double]("a"   -> 1, "b" -> 2)
-    val aNeighbours     = Map[String, Double]("fin" -> 1)
-    val bNeighbours     = Map[String, Double]("a"   -> 3, "fin" -> 5)
 
-    val graph: Map[String, Map[String, Double]] =
-      Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours)
-
-    Dijkstra(graph) shouldBe 2
-  }
 }
