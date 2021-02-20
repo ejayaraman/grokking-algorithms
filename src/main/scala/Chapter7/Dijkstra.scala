17a551f95c5021067413f44a57687dacd07ad5c2
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-02-20 20:17:26 +0000

    Added additional test and fixed bug.

--- src/main/scala/Chapter7/Dijkstra.scala
+++ src/main/scala/Chapter7/Dijkstra.scala
@@ -2,11 +2,13 @@ package Chapter7
 
 import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 //Second Iteration
 object Dijkstra {
   def apply(graph: Map[String, List[Node]]): Int = {
-    val costs: List[Node]                    = graph("start") ++ List(Node("final", Double.PositiveInfinity.toInt))
+    val costs: ArrayBuffer[Node] = new ArrayBuffer[Node]() ++ (graph("start") ++ List(
+      Node("final", Double.PositiveInfinity.toInt)))
     val parents: mutable.Map[String, String] = mutable.Map[String, String]()
     graph("start").foreach { value =>
       parents.addOne(value.name -> "start")
@@ -14,27 +16,33 @@ object Dijkstra {
 
     @tailrec
     def recurse(processedNodes: List[String]): Int = {
-      val unprocessedNodes: List[Node] = costs.filterNot(node => processedNodes.contains(node.name))
+      val unprocessedNodes: List[Node] = costs.filterNot(node => processedNodes.contains(node.name)).toList
       val lowestCostNode = unprocessedNodes match {
         case Nil          => None
         case head :: tail => Some((head :: tail).minBy(_.edge))
       }
 
       lowestCostNode match {
-        case Some(node) =>
-          val neighbours: Option[List[Node]] = graph.get(node.name)
-          neighbours.foreach { neighbour =>
-            neighbour.foreach { n =>
-              val currentNode = costs.find(_.name == n.name)
-              val currentCost = currentNode.get.edge
-              val newCost     = costs.find(_.name == node.name).get.edge + n.edge
-              if (currentCost > newCost) {
-                currentNode.get.edge = newCost
-                parents.update(n.name, node.name)
+        case Some(lowCostNode) =>
+          graph.get(lowCostNode.name) match {
+            case Some(neighbours) =>
+              neighbours.foreach { n =>
+                val currentNode = if (costs.exists(_.name == n.name)) {
+                  costs.find(_.name == n.name).get
+                } else {
+                  costs.addOne(Node(n.name, Double.PositiveInfinity.toInt)) //add a node to the costs collection when not exists.
+                  costs.find(_.name == n.name).get
+                }
+                val currentCost = currentNode.edge
+                val newCost     = costs.find(_.name == lowCostNode.name).get.edge + n.edge
+                if (currentCost > newCost) {
+                  currentNode.edge = newCost
+                  parents.update(n.name, lowCostNode.name)
+                }
               }
-            }
+              recurse(lowCostNode.name :: processedNodes)
+            case None => recurse(lowCostNode.name :: processedNodes)
           }
-          recurse(node.name :: processedNodes)
         case None => costs.find(_.name == "final").get.edge
       }
     }
--- src/test/scala/chapter7/DijkstraSpec.scala
+++ src/test/scala/chapter7/DijkstraSpec.scala
@@ -28,5 +28,27 @@ class DijkstraSpec extends AnyFlatSpec with Matchers {
     Dijkstra(graph) shouldBe 6
   }
 
+  "Dijkstra" should "return the shortest path when there is a node that is not connected to final" in {
+    val startNeighbours = List[Node](Node("a", 1), Node("b", 2))
+    val aNeighbours     = List[Node](Node("final", 1))
+    val bNeighbours     = List[Node](Node("a", 3), Node("c", 1), Node("final", 5))
+
+    val graph: Map[String, List[Node]] =
+      Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours)
+
+    Dijkstra(graph) shouldBe 2
+  }
+
+  "Dijkstra" should "return the shortest path via C" in {
+    val startNeighbours = List[Node](Node("a", 1), Node("b", 2))
+    val aNeighbours     = List[Node](Node("final", 6))
+    val bNeighbours     = List[Node](Node("a", 3), Node("c", 1), Node("final", 10))
+    val cNeighbours     = List[Node](Node("final", 1))
+
+    val graph: Map[String, List[Node]] =
+      Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours, "c" -> cNeighbours)
+
+    Dijkstra(graph) shouldBe 4
+  }
 
 }
