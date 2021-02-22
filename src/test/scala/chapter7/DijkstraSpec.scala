cbc7c9aeda4b8e77cbcaedd25121d2c0acd8d649
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-02-22 21:31:59 +0000

    Additional Tests

--- src/test/scala/chapter7/DijkstraSpec.scala
+++ src/test/scala/chapter7/DijkstraSpec.scala
@@ -51,4 +51,29 @@ class DijkstraSpec extends AnyFlatSpec with Matchers {
     Dijkstra(graph) shouldBe 4
   }
 
+  "Dijkstra" should "return the shortest path for exercise 7.1A" in {
+    val startNeighbours = List[Node](Node("a", 5), Node("b", 2))
+    val aNeighbours     = List[Node](Node("c", 4), Node("d", 2))
+    val bNeighbours     = List[Node](Node("a", 8), Node("d", 7))
+    val cNeighbours     = List[Node](Node("d", 6), Node("final", 3))
+    val dNeighbours     = List[Node](Node("final", 1))
+
+    val graph: Map[String, List[Node]] =
+      Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours, "c" -> cNeighbours, "d" -> dNeighbours)
+
+    Dijkstra(graph) shouldBe 8
+  }
+
+  "Dijkstra" should "return the shortest path for exercise 7.1B" in {
+    val startNeighbours = List[Node](Node("a", 10))
+    val aNeighbours     = List[Node](Node("b", 20))
+    val bNeighbours     = List[Node](Node("c", 1), Node("final", 30))
+    val cNeighbours     = List[Node](Node("a", 1))
+
+    val graph: Map[String, List[Node]] =
+      Map("start" -> startNeighbours, "a" -> aNeighbours, "b" -> bNeighbours, "c" -> cNeighbours)
+
+    Dijkstra(graph) shouldBe 60
+  }
+
 }
