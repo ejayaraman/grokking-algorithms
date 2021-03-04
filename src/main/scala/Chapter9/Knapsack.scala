7124b7e28f9dcd363963fdb101921a838a88601d
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-03-04 21:03:41 +0000

    Knapsack - Second Iteration

--- src/main/scala/Chapter9/Knapsack.scala
+++ src/main/scala/Chapter9/Knapsack.scala
@@ -1,8 +1,7 @@
 package Chapter9
 
-//First Iteration
+//Second Iteration
 object Knapsack {
-
   def apply(items: List[Item], knapsackSize: Int): Int = {
     val table: Array[Array[Int]] = Array.ofDim[Int](items.size, knapsackSize)
 
@@ -11,30 +10,20 @@ object Knapsack {
       size          <- 0 until knapsackSize
     } {
       val sackSize = size + 1
-      if (index == 0) {
+      table(index)(size) = if (index == 0) {
         if (item.weight <= sackSize)
-          table(index)(size) = item.value
-        else {
-          table(index)(size) = 0
-        }
+          item.value
+        else
+          0
       } else {
-        if (item.weight == sackSize) {
-          if (item.value > table(index - 1)(sackSize)) {
-            table(index)(size) = item.value
-          } else {
-            table(index)(size) = table(index - 1)(size)
-          }
-        }
-        if (item.weight > sackSize) {
-          table(index)(size) = table(index - 1)(size)
-        }
-        if (item.weight < sackSize) {
-          if (item.value + table(index - 1)(size - item.weight) > table(index - 1)(size)) {
-            table(index)(size) = item.value + table(index - 1)(size - item.weight)
-          } else {
-            table(index)(size) = table(index - 1)(size)
-          }
-        }
+        if (item.weight == sackSize && item.value > table(index - 1)(sackSize))
+          item.value
+        else if (item.weight > sackSize)
+          table(index - 1)(size)
+        else if (item.weight < sackSize && (item.value + table(index - 1)(size - item.weight) > table(index - 1)(size)))
+          item.value + table(index - 1)(size - item.weight)
+        else
+          table(index - 1)(size)
       }
       println(index, size, item.name, item.weight, item.value, table(index)(size))
     }
